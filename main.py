2b8d4abf00a8173e8a3b1811ab956f2e49eba16d
Chi Han <han_chi0727@outlook.com>
2024-05-23 16:57:55 +0800

    better code formatting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import ImageFilter,ImageEnhance,ExifTags,Image
 from PIL import ImageDraw, ImageFont
+from pillow_heif import register_heif_opener
 from PIL.ExifTags import TAGS
 import numpy as np
 import os
@@ -41,15 +42,22 @@ def processPhoto(im_name, shrink_f=0.6514, sourceFolder="./source/", outputFolde
     # Creating New Canvas
     ## Scale relative to the shrinked image, I set it to be fixed
     ## Feel Free to change if needed
-    scale = [1.15,1.35] 
-    canvas_size = [int(width*shrink_factor*scale[0]), int(height*shrink_factor*scale[1])]
+    # Creating New Canvas
+    scale = np.array([1.15,1.35])
+    portrait_factor = 1
     
-    # Getting the longer side of the canvas
-    max_edge = max(canvas_size) 
+    if width < height: 
+        portrait_factor = 1.1
+        scale = [1.15,1.30]
+
+    canvas_size = [int(width*shrink_factor*scale[0]), int(height*shrink_factor*scale[1])]
+
+    max_edge = max(canvas_size)
+
     canvas = Image.new('RGB', (canvas_size[0], canvas_size[1]), (255,255,219))
 
     # Creating background
-    enlarged_image = image.resize((int(max_edge/height*width),max_edge))
+    enlarged_image = image.resize((int(max_edge/height*width), max_edge))
     # Blurring background
     enlarged_image_blurred = enlarged_image.filter(ImageFilter.GaussianBlur(25))
     # Dimming Background
@@ -67,8 +75,10 @@ def processPhoto(im_name, shrink_f=0.6514, sourceFolder="./source/", outputFolde
     cropped = enlarged_image_blurred.crop((left, top, right, bottom))
 
     # Checking if the cropped background fits the canvas
+    # print(list(cropped.size), canvas_size)
     if not list(cropped.size) == canvas_size: 
-        raise("False")
+        pass
+        # raise("False")
 
     # Pasting background
     canvas.paste(cropped)
@@ -77,38 +87,44 @@ def processPhoto(im_name, shrink_f=0.6514, sourceFolder="./source/", outputFolde
     canvas.paste(new_image, (position[0], position[1]))
 
     # Custom font style and font size
-    size1 = int(max(width*shrink_factor,height*shrink_factor)*(78/3376)+0.5)
-    size2 = int(60/75*size1+0.5)
-    size3 = int(260/75*size1+0.5)
+    # Custom font style and font size
+    size1 = int(max(width*shrink_factor,height*shrink_factor)*(75/3376)*portrait_factor+0.5)
+    size2 = int(60/75*size1*portrait_factor+0.5)
+    size3 = int(260/75*size1*portrait_factor+0.5)
 
+    # Fonts
     cmu_serif = ImageFont.truetype('./cmunui.ttf', size1)
     # cmu_bold = ImageFont.truetype('./cmunbx.ttf', 60)
     cmu_bold = ImageFont.truetype('./cmunsx.ttf', size2)
     cmu_tt = ImageFont.truetype('./cmuntt.ttf', size1)
     cmu_bold2 = ImageFont.truetype('./cmunbx.ttf',size3)
+    charlottes = ImageFont.truetype('./Charlotte.otf',int(size3*0.75))
 
     # Setting Splitting
-    split1 = int(80/75*size1+0.5)
-    split2 = int(100/75*size1+0.5)
+    split1 = int(80/75*size1*portrait_factor+0.5)
+    split2 = int(100/75*size1*portrait_factor+0.5)
+    split3 = int(split2*0.6*portrait_factor)
 
     text_top = np.array(position.copy()) + np.array([0,new_size[1]])
+
+    # canvas.save(f"./output/{imagename[:-4]}_o_NO.png")
 
     # Adding texts
     canvas_draw = ImageDraw.Draw(canvas)
     # print(exif_dict["LensModel"])
 
     # Lens Model
-    canvas_draw.text((text_top[0], text_top[1]+split2+split1), text = exif_dict["Make"] + " " + exif_dict["Model"], font=cmu_bold, fill =(255, 255, 255))
+    canvas_draw.text((text_top[0], text_top[1]+split3+split1), text = exif_dict["Make"] + " " + exif_dict["Model"], font=cmu_bold, fill =(255, 255, 255))
     # Camera Make
-    canvas_draw.text((text_top[0], text_top[1]+split2+split1*2), text = exif_dict["LensModel"], font=cmu_bold, fill=(255, 255, 255))
+    canvas_draw.text((text_top[0], text_top[1]+split3+split1*2), text = exif_dict["LensModel"], font=cmu_bold, fill=(255, 255, 255))
     # Date & Time
-    canvas_draw.text((text_top[0], text_top[1]+split2+split1*3), text = exif_dict["DateTime"].replace(":", "-"), font=cmu_bold, fill=(255, 255, 255))
+    canvas_draw.text((text_top[0], text_top[1]+split3+split1*3), text = exif_dict["DateTimeOriginal"].replace(":", "-"), font=cmu_bold, fill=(255, 255, 255))
 
     w = int(split1/15+0.5)
 
     # Splitting Line
-    canvas_draw.line(xy=[(text_top[0], text_top[1]+split2+split1*3+split2), 
-                        (text_top[0]+int(12.5*split2), text_top[1]+split2+split1*3+split2)], 
+    canvas_draw.line(xy=[(text_top[0], text_top[1]+split3+split1*3+1.4*split2), 
+                        (text_top[0]+int(13.5*split2), text_top[1]+split3+split1*3+1.4*split2)], 
                     width=w,
                     fill=(255, 255, 255))
 
@@ -121,7 +137,7 @@ def processPhoto(im_name, shrink_f=0.6514, sourceFolder="./source/", outputFolde
         exposure_t += str(int(1/exif_dict["ExposureTime"]))
 
 
-    canvas_draw.text((text_top[0], text_top[1]+split2+split1*3+split2), 
+    canvas_draw.text((text_top[0], text_top[1]+split3+split1*3+1.4*split2), 
                     text = "ISO " + str(exif_dict["ISOSpeedRatings"]) + " | " 
                     + "F" + str(exif_dict["FNumber"]) + " | " 
                     + exposure_t + " s" + " | "
@@ -147,9 +163,9 @@ def processPhoto(im_name, shrink_f=0.6514, sourceFolder="./source/", outputFolde
                         text = "LOGO",
                         font=cmu_bold2, fill =(255, 255, 255))
     elif sign:
-        canvas_draw.text((np.array(c_box[2])[0], text_top[1]+split2+split1*3+split2), 
-                        text = "\u00A9 Suzutsuki-CH",
-                        font=cmu_serif, fill =(255, 255, 255))
+        canvas_draw.text((np.array(c_box[2])[0] - 250, text_top[1]+200), 
+                        text = "Suzutsuki-CH",
+                        font=charlottes, fill =(255, 255, 255))
     # Saving the photo
     canvas.save(f"./output/{imagename[:-4]}_o.png")
 
@@ -166,8 +182,11 @@ def main():
     outputFolder = input() + "/"
     """
 
+    register_heif_opener()
+
     sourceFolder="./source/"
     PhotoList = os.listdir(sourceFolder)
+    PhotoList.remove('.DS_Store')
 
     print("Enter shrink factor (between 0 to 1): ")
     sf = float(input())
@@ -177,7 +196,13 @@ def main():
 
     if all_export.capitalize() == "Y":
         for im_name in tqdm(PhotoList):
-            processPhoto(im_name, shrink_f=sf, sourceFolder="./source/", outputFolder="./output/", suffix="_o")
+            try:
+                processPhoto(im_name, shrink_f=sf, sourceFolder="./source/", outputFolder="./output/", suffix="_o")
+            except: 
+                print(f"Unable to process: {im_name}")
+                continue
+            
+
     elif all_export.capitalize() == "N":
         print(f"Enter the index of number you want to process (0 to {str(len(PhotoList)-1)}):")
         idx = int(input())
